5aa29021f6c4df0d0484f24644164172a8f769d5
george <george.williams.business@gmail.com>
2023-11-06 07:11:19 +1000

    cleared up unused variables

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -4,10 +4,8 @@ import { Store } from "../types";
 export const useMovieStore = create((set) => ({
   searchQuery: "",
   searchResults: [],
-  filePath: "",
   cardBaseUrl: "",
   backDropBaseUrl: "",
-  loadingData: false,
   setSearchResults: (newSearchResults: any) =>
     set(() => ({ searchResults: newSearchResults })),
   setConfig: (newBackDropBaseUrl: string, newCardBaseUrl: string) =>
